a66246b70ed49e3ad005c3612172157b4e053216
rag16c <44474646+rag16c@users.noreply.github.com>
2018-12-04 13:01:06 -0500

    Update search_page.php

--- search_page.php
+++ search_page.php
@@ -7,6 +7,9 @@ $servername="localhost";
 $username="klopfens";
 $password="P0za4TSlbSTSq58v";
 
+$search_table=$_POST['tableSelect'];
+$search_field=$_POST['fieldSelect'];
+$search_value=$_POST['keyword'];
 //Create a connection
 $conn= new mysqli($servername,$username,$password);
 
@@ -15,7 +18,16 @@ if($conn->connect_error){
 	die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected succesfully";
-
+	
+if ( $search_table == "all" )
+{
+	$sql = "select 'search_field' from * where 'search_field' like '%search_value%'";
+	$res = $con->query($sql);
+		
+	while ( $row=$res->fetch_assoc()) {
+		echo $search_value, ' ',$row['search_field'];
+	}
+}
 ?>
 
 </body>
