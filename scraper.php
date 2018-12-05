81edaec5bd4ea054788a1eb8812515c08fca1e9c
aao17b <aao17b@my.fsu.edu>
2018-12-05 16:40:13 -0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,20 +1,21 @@
 <?php
-
-require('simple_html_dom.php');
-
-$html = file_get_html(/*youtube link*/);
-// $owner = $html->find('#owner-name.a.yt-simple-endpoint.style-scope.yt-formatted-string')
-// $owner = $html->find('yt-formatted-string#owner-name.style-scope.ytd-video-owner-renderer');
-// $ownerName = $owner->
-// $ownerAddress = 'https://youtube.com' . $owner->href;
-
-$owner = $html->find('img#img.style-scope.yt-imd-shadow');
-$ownerName = $owner->aria-label;
-$ownerAddress = 'https://youtube.com' . $owner->href;
-
-$videoTitle = $html->find('yt-formatted-string.style-scope.ytd-video-primary-info-renderer');
-$videoPubDate = $html->find('span.date.style-scope.trd-video-secondary-info-renderer');
+	// Need to set $link to the youtube video URL
 
 
-var_dump($ownerName, $ownerAddress, $videoTitle, $videoPubDate);
+	require('simple_html_dom.php');
+	$html = file_get_html($link);
+	// $owner = $html->find('#owner-name.a.yt-simple-endpoint.style-scope.yt-formatted-string')
+	// $owner = $html->find('yt-formatted-string#owner-name.style-scope.ytd-video-owner-renderer');
+	// $ownerName = $owner->
+	// $ownerAddress = 'https://youtube.com' . $owner->href;
+
+	$owner = $html->find('img#img.style-scope.yt-imd-shadow');
+
+	$ownerName = $owner->aria-label;
+	$ownerAddress = 'https://youtube.com' . $owner->href;
+
+	$videoTitle = $html->find('yt-formatted-string.style-scope.ytd-video-primary-info-renderer');
+	$videoPubDate = $html->find('span.date.style-scope.trd-video-secondary-info-renderer');
+
+
 ?>
