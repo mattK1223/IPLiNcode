2b0e9b8c72930d3ef0435372fb981039df233896
aao17b <aao17b@my.fsu.edu>
2018-12-05 16:16:01 -0500

    Added some PHP for webscraping.

---
+++ scraper.php
@@ -0,0 +1,20 @@
+<?php
+
+require('simple_html_dom.php');
+
+$html = file_get_html(/*youtube link*/);
+// $owner = $html->find('#owner-name.a.yt-simple-endpoint.style-scope.yt-formatted-string')
+// $owner = $html->find('yt-formatted-string#owner-name.style-scope.ytd-video-owner-renderer');
+// $ownerName = $owner->
+// $ownerAddress = 'https://youtube.com' . $owner->href;
+
+$owner = $html->find('img#img.style-scope.yt-imd-shadow');
+$ownerName = $owner->aria-label;
+$ownerAddress = 'https://youtube.com' . $owner->href;
+
+$videoTitle = $html->find('yt-formatted-string.style-scope.ytd-video-primary-info-renderer');
+$videoPubDate = $html->find('span.date.style-scope.trd-video-secondary-info-renderer');
+
+
+var_dump($ownerName, $ownerAddress, $videoTitle, $videoPubDate);
+?>
